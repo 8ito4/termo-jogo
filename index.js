7cce317c33f4fb4b262d1d08747a447e56d17e6f
hackathonbee2020 <70024254+hackathonbee2020@users.noreply.github.com>
2025-07-30 11:20:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//inciar o express (framework Node.js)
+const app = require('express')();
+
+// Mostrar a porta que o servidor está rodando
+app.listen(3000, () => {console.log('Servidor rodando na porta 3000')});
+
+// Rota para a página inicial
+app.get('/skills', (req, res) => {
+    res.send('Olá Mundo! Javascript e Node.js');
+});
+
+// Rota para a página inicial
+app.get('/objeto', (req, res) => {
+    res.send({
+        javascript: '100%',
+        node: '100%',
+        react: '100%',
+        next: '100%',
+        typescript: '100%',
+        express: '100%',
+        mongodb: '100%'
+    });
+});
+
+
